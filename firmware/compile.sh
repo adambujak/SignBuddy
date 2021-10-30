eb112fa3116319422003d445d9d9b93f13788a70
Stephen Scott <stephen99scott@gmail.com>
2021-10-30 12:46:43 -0400

    New scripts to build src in docker w or w/o new dependencies

---
+++ firmware/compile.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+docker create -it --name env buildenv bash
+docker start env
+docker exec -i env bash -c "cd /workspace/firmware && rm -rf applications libraries"
+docker cp -a applications env:/workspace/firmware/
+docker cp -a libraries env:/workspace/firmware/
+docker exec -i env bash -c "cd /workspace/firmware/applications/SignBuddy && make"
+docker cp -a env:/workspace/firmware/applications/SignBuddy/_build applications/SignBuddy/_build
+docker rm -f env
