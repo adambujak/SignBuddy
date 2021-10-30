a5428017901c2ccc78c2933204ceb8f15520e1a1
Stephen Scott <stephen99scott@gmail.com>
2021-10-30 19:51:21 -0400

    Removed redundancies and fixed windows mingw bug

--- firmware/compile.sh
+++ firmware/compile.sh
@@ -7,7 +7,7 @@ if ! docker image inspect ${IMAGE_NAME} &>> /dev/null; then
     docker build . -t buildenv
 fi
 
-docker create -it -v ${pwd}:/workspace/firmware --name env ${IMAGE_NAME} bash > /dev/null
+docker create -it -v /$(pwd):/workspace --name env ${IMAGE_NAME} bash > /dev/null
 docker start env > /dev/null
-docker exec -i env bash -c "cd /workspace/firmware/applications/SignBuddy && make"
+docker exec -i env bash -c "cd /workspace/applications/SignBuddy && make"
 docker rm -f env > /dev/null
