ea311b0df2a7abb1f1ac236ab488a2551c4bc08a
Adam Bujak <adamjbujak@gmail.com>
2021-10-30 18:19:28 -0400

    change dockerfile scheme to just mount the build directory instead of copying everything

--- firmware/compile.sh
+++ firmware/compile.sh
@@ -1,10 +1,6 @@
 #! /bin/sh
 
-docker create -it --name env buildenv bash
+docker create -it -v $(pwd):/workspace/firmware --name env buildenv bash
 docker start env
-docker exec -i env bash -c "cd /workspace/firmware && rm -rf applications libraries"
-docker cp -a applications env:/workspace/firmware/
-docker cp -a libraries env:/workspace/firmware/
 docker exec -i env bash -c "cd /workspace/firmware/applications/SignBuddy && make"
-docker cp -a env:/workspace/firmware/applications/SignBuddy/_build applications/SignBuddy/
 docker rm -f env
