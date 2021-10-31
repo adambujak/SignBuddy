72c56adfe800216135b9a010d126ff050f269ff2
Adam Bujak <adamjbujak@gmail.com>
2021-10-31 15:37:34 -0400

    clean up and abstract shell scripts to be path agnostic

--- firmware/tools/compile.sh
+++ firmware/tools/compile.sh
@@ -1,13 +1,15 @@
-#! /bin/sh
+#! /bin/bash
 
 IMAGE_NAME="buildenv"
+GIT_ROOT=$(git rev-parse --show-toplevel)
+FIRMWARE_ROOT=${GIT_ROOT}/firmware
 
 if ! docker image inspect ${IMAGE_NAME} &> /dev/null; then
     echo "Docker image does not exist.. building now"
     docker build . -t buildenv
 fi
 
-docker create -it -v /$(pwd):/workspace --name env ${IMAGE_NAME} bash > /dev/null
+docker create -it -v ${FIRMWARE_ROOT}:/workspace --name env ${IMAGE_NAME} bash > /dev/null
 docker start env > /dev/null
 docker exec -i env bash -c "cd /workspace/applications/SignBuddy && make"
 docker rm -f env > /dev/null
--- firmware/tools/run_formatter.sh
+++ firmware/tools/run_formatter.sh
@@ -1,15 +1,18 @@
 #! /bin/sh
 
 IMAGE_NAME="buildenv"
+GIT_ROOT=$(git rev-parse --show-toplevel)
+FIRMWARE_ROOT=${GIT_ROOT}/firmware
+TEMP_FIND_FILE=/workspace/formatter_files.txt
 
 if ! docker image inspect ${IMAGE_NAME} &> /dev/null; then
     echo "Docker image does not exist.. building now"
     docker build . -t buildenv
 fi
 
-docker create -it -v /$(pwd):/workspace --name env ${IMAGE_NAME} bash > /dev/null
+docker create -it -v ${FIRMWARE_ROOT}:/workspace --name env ${IMAGE_NAME} bash > /dev/null
 docker start env > /dev/null
-docker exec -i env bash -c "find /workspace/. \( -name '*.c' -o -name '*.h' -o -path '/workspace/./dependencies' -prune \) -type f > /workspace/formatter_files.txt"
-docker exec -i env bash -c "uncrustify -c /workspace/firmware_style.cfg -F /workspace/formatter_files.txt --replace --no-backup"
-docker exec -i env bash -c "rm /workspace/formatter_files.txt"
+docker exec -i env bash -c "find /workspace/. \( -name '*.c' -o -name '*.h' -o -path '/workspace/./dependencies' -prune \) -type f > ${TEMP_FIND_FILE}"
+docker exec -i env bash -c "uncrustify -c /workspace/tools/firmware_style.cfg -F ${TEMP_FIND_FILE} --replace --no-backup"
+docker exec -i env bash -c "rm ${TEMP_FIND_FILE}"
 docker rm -f env > /dev/null
