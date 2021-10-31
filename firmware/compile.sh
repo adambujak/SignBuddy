7a03c4567e214fd98ee04332a8d5188e8065bc89
Stephen Scott <stephen99scott@gmail.com>
2021-10-30 22:21:01 -0400

    Added dockerfile for compiling/formatting source code

---
+++ firmware/compile.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+IMAGE_NAME="buildenv"
+
+if ! docker image inspect ${IMAGE_NAME} &>> /dev/null; then
+    echo "Docker image does not exist.. building now"
+    docker build . -t buildenv
+fi
+
+docker create -it -v /$(pwd):/workspace --name env ${IMAGE_NAME} bash > /dev/null
+docker start env > /dev/null
+docker exec -i env bash -c "cd /workspace/applications/SignBuddy && make"
+docker rm -f env > /dev/null
