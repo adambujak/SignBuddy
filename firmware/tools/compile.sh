0127c534636223db1625b90b7250dc3c9fb00c87
Adam Bujak <adamjbujak@gmail.com>
2021-11-12 21:47:29 -0500

    change the way boards are built

--- firmware/tools/compile.sh
+++ firmware/tools/compile.sh
@@ -9,9 +9,7 @@ FIRMWARE_ROOT=$GIT_ROOT/firmware
 BUILD_DOCKER_SCRIPT=$FIRMWARE_ROOT/tools/build_docker_image.sh
 
 source "$BUILD_DOCKER_SCRIPT"
-if [[ -n "$is_nucleo" ]]; then
-	docker create -it -e BOARD_VERSION=$1 -v $FIRMWARE_ROOT:/workspace --name env $IMAGE_NAME bash > /dev/null
-fi
+
 docker create -it -v $FIRMWARE_ROOT:/workspace --name env $IMAGE_NAME bash > /dev/null
 docker start env > /dev/null
 docker exec -i env bash -c "cd /workspace/applications/SignBuddy && make BOARD_VERSION=$BOARD_VERSION"
