a9a2c895a7df1a6ceb2464e944154cb9ba618255
Adam Bujak <adamjbujak@gmail.com>
2021-11-15 22:07:41 -0500

    delete docker container if exists when running scripts (#31)
    * delete docker container if exists
    
    * fix var name
    
    * add fi
    
    * fix

--- firmware/tools/build_docker_image.sh
+++ firmware/tools/build_docker_image.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 
 IMAGE_NAME="buildenv"
+CONTAINER_NAME="env"
 
 GIT_ROOT=$(git rev-parse --show-toplevel)
 FIRMWARE_ROOT=$GIT_ROOT/firmware
@@ -11,4 +12,8 @@ if ! docker image inspect $IMAGE_NAME &> /dev/null; then
     docker build $DOCKER_PATH -t $IMAGE_NAME
 fi
 
-
+if docker container inspect $CONTAINER_NAME &> /dev/null; then
+    echo "Deleting docker container... "
+    docker container stop $CONTAINER_NAME
+    docker container rm $CONTAINER_NAME
+fi
