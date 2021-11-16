85bbe68c0a0459ab3c9f26808b2ca4b17e7f6ac1
Adam Bujak <adamjbujak@gmail.com>
2021-11-15 19:41:03 -0500

    fix

--- firmware/tools/build_docker_image.sh
+++ firmware/tools/build_docker_image.sh
@@ -12,7 +12,7 @@ if ! docker image inspect $IMAGE_NAME &> /dev/null; then
     docker build $DOCKER_PATH -t $IMAGE_NAME
 fi
 
-if ! docker container inspect $CONTAINER_NAME &> /dev/null; then
+if docker container inspect $CONTAINER_NAME &> /dev/null; then
     echo "Deleting docker container... "
     docker container stop $CONTAINER_NAME
     docker container rm $CONTAINER_NAME
