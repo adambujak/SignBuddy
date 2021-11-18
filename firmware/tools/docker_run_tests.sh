158d6222694db83e6b00666ee2421b680501437a
Adam Bujak <adamjbujak@gmail.com>
2021-11-18 14:24:47 -0500

    add docker test runner

---
+++ firmware/tools/docker_run_tests.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+GIT_ROOT=$(git rev-parse --show-toplevel)
+FIRMWARE_ROOT=$GIT_ROOT/firmware
+BUILD_DOCKER_SCRIPT=$FIRMWARE_ROOT/tools/build_docker_image.sh
+
+startDocker ()
+{
+    local IMAGE_NAME="buildenv"
+
+    source "$BUILD_DOCKER_SCRIPT"
+
+    docker create -it -v $GIT_ROOT:/workspace --name env $IMAGE_NAME bash > /dev/null
+    docker start env > /dev/null
+}
+
+cleanupDocker ()
+{
+    docker rm -f env > /dev/null
+}
+
+runTests ()
+{
+    docker exec -i env bash -c "pushd /workspace/firmware/test > /dev/null && bash run_tests.sh $@"
+    TEST_RUN_STATUS=$?
+}
+
+startDocker
+runTests
+cleanupDocker
+
+exit $TEST_RUN_STATUS
