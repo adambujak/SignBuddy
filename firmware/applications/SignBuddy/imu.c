952e7e0218d4bf4d0a95714b3f199100a423e25b
HARNEET <singhh76@mcmaster.ca>
2021-11-20 18:35:17 -0500

    dev_addr changed

--- firmware/applications/SignBuddy/imu.c
+++ firmware/applications/SignBuddy/imu.c
@@ -51,13 +51,13 @@ static void hw_init(void)
 
 static inline int8_t write(uint8_t dev_addr, uint8_t reg_addr, uint8_t *reg_data, uint8_t cnt)
 {
-  i2c_write(&s.i2c_instance, dev_addr, reg_addr, reg_data, (uint16_t)cnt);
+  i2c_write(&s.i2c_instance, dev_addr << 1, reg_addr, reg_data, (uint16_t)cnt);
   return 0;
 }
 
 static inline int8_t read(uint8_t dev_addr, uint8_t reg_addr, uint8_t *reg_data, uint8_t cnt)
 {
-  i2c_read(&s.i2c_instance, dev_addr, reg_addr, reg_data, (uint16_t)cnt);
+  i2c_read(&s.i2c_instance, dev_addr << 1, reg_addr, reg_data, (uint16_t)cnt);
   return 0;
 }
 
