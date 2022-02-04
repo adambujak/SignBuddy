d123113ed32943ac40f66e02a6a7afef7baba9a1
Adam Bujak <adamjbujak@gmail.com>
2022-02-04 10:30:52 -0500

    reduce logger buffer sizes

--- firmware/applications/SignBuddy/log_uart.c
+++ firmware/applications/SignBuddy/log_uart.c
@@ -8,10 +8,8 @@
 #include <stdbool.h>
 #include <string.h>
 
-static uint8_t rx_buffer[LOG_UART_RX_BUFFER_SIZE];
 static uint8_t tx_buffer[LOG_UART_TX_BUFFER_SIZE];
 
-static fifo_t rx_fifo;
 static fifo_t tx_fifo;
 
 static bool writing = false;
@@ -45,7 +43,6 @@ static void hw_init(void)
   LL_USART_ConfigAsyncMode(LOG_UART);
   LL_USART_Enable(LOG_UART);
 
-  LL_USART_EnableIT_RXNE(LOG_UART);
   LL_USART_EnableIT_TC(LOG_UART);
 
   NVIC_SetPriority(LOG_UART_IRQn, LOG_UART_PRIORITY);
@@ -77,31 +74,15 @@ void log_uart_write(uint8_t *data, uint32_t length)
   ENABLE_IRQ();
 }
 
-int log_uart_read(uint8_t *data, uint32_t length)
-{
-  DISABLE_IRQ();
-  int ret = fifo_pop(&rx_fifo, data, length);
-  ENABLE_IRQ();
-  return ret;
-}
-
 void log_uart_init(void)
 {
   hw_init();
-  fifo_init(&rx_fifo, rx_buffer, LOG_UART_RX_BUFFER_SIZE);
   fifo_init(&tx_fifo, tx_buffer, LOG_UART_TX_BUFFER_SIZE);
 }
 
 void LOG_UART_IRQHandler(void)
 {
   DISABLE_IRQ();
-  uint8_t data;
-  if (LL_USART_IsActiveFlag_RXNE(LOG_UART)) {
-    data = LL_USART_ReceiveData8(LOG_UART);
-    // TODO: make sure this is correct flag to clear
-    LL_USART_ClearFlag_NE(LOG_UART);
-    fifo_push(&rx_fifo, &data, 1);
-  }
   if (LL_USART_IsActiveFlag_TC(LOG_UART)) {
     writing = false;
     LL_USART_ClearFlag_TC(LOG_UART);
