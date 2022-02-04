a1154f957fcc6e817035a1e9b5e2042c4e76a9a7
Stephen Scott <stephen99scott@gmail.com>
2022-02-03 21:29:22 -0500

    Add error handling for I2C

--- firmware/applications/SignBuddy/i2c.c
+++ firmware/applications/SignBuddy/i2c.c
@@ -3,24 +3,58 @@
 #include "common.h"
 #include "logger.h"
 
-static void write_byte(I2C_TypeDef *handle, const uint8_t *tx_data)
+#define I2C_MAX_TIMEOUT    10
+
+static uint8_t write_byte(I2C_TypeDef *handle, const uint8_t *tx_data)
 {
+  uint8_t timeout = I2C_MAX_TIMEOUT;
+
   if (LL_I2C_IsActiveFlag_TXE(handle)) {
     LL_I2C_TransmitData8(handle, *tx_data);
-    while (!LL_I2C_IsActiveFlag_TXE(handle) && !LL_I2C_IsActiveFlag_STOP(handle));
+    while (!LL_I2C_IsActiveFlag_TXE(handle) && !LL_I2C_IsActiveFlag_STOP(handle)) {
+      if (LL_SYSTICK_IsActiveCounterFlag()) {
+        if (timeout-- == 0) {
+          LL_I2C_GenerateStopCondition(handle);
+          LOG_ERROR("I2C: Timeout on write\r\n");
+          return RET_ERR;
+        }
+      }
+    }
   }
+  else {
+    LOG_ERROR("I2C: Transmit register full\r\n");
+    return RET_ERR;
+  }
+
+  return RET_OK;
 }
 
-static void read_byte(I2C_TypeDef *handle, uint8_t *rx_data)
+static uint8_t read_byte(I2C_TypeDef *handle, uint8_t *rx_data)
 {
-  while (!LL_I2C_IsActiveFlag_RXNE(handle) && !LL_I2C_IsActiveFlag_STOP(handle));
+  uint8_t timeout = I2C_MAX_TIMEOUT;
+
+  while (!LL_I2C_IsActiveFlag_RXNE(handle) && !LL_I2C_IsActiveFlag_STOP(handle)) {
+    if (LL_SYSTICK_IsActiveCounterFlag()) {
+      if (timeout-- == 0) {
+        LL_I2C_GenerateStopCondition(handle);
+        LOG_ERROR("I2C: Timeout on read\r\n");
+        return RET_ERR;
+      }
+    }
+  }
 
   if (LL_I2C_IsActiveFlag_RXNE(handle)) {
     *rx_data = LL_I2C_ReceiveData8(handle);
   }
+  else {
+    LOG_ERROR("I2C: Receive register already read");
+    return RET_ERR;
+  }
+
+  return RET_OK;
 }
 
-int i2c_write(i2c_t *instance, uint8_t slave_addr, uint8_t reg_addr, const uint8_t *data, uint16_t length)
+uint8_t i2c_write(i2c_t *instance, uint8_t slave_addr, uint8_t reg_addr, const uint8_t *data, uint16_t length)
 {
   I2C_TypeDef *handle = (I2C_TypeDef *) instance->handle;
 
@@ -31,19 +65,19 @@ int i2c_write(i2c_t *instance, uint8_t slave_addr, uint8_t reg_addr, const uint8
                         LL_I2C_MODE_AUTOEND,
                         LL_I2C_GENERATE_START_WRITE);
 
-  write_byte(handle, &reg_addr);
+  ERR_CHECK(write_byte(handle, &reg_addr));
 
   for (uint32_t i = 0; i < length; i++) {
-    write_byte(handle, &data[i]);
+    ERR_CHECK(write_byte(handle, &data[i]));
   }
 
   while (!LL_I2C_IsActiveFlag_STOP(handle));
   LL_I2C_ClearFlag_STOP(handle);
 
-  return 0;
+  return RET_OK;
 }
 
-int i2c_read(i2c_t *instance, uint8_t slave_addr, uint8_t reg_addr, uint8_t *data, uint16_t length)
+uint8_t i2c_read(i2c_t *instance, uint8_t slave_addr, uint8_t reg_addr, uint8_t *data, uint16_t length)
 {
   I2C_TypeDef *handle = (I2C_TypeDef *) instance->handle;
 
@@ -57,13 +91,13 @@ int i2c_read(i2c_t *instance, uint8_t slave_addr, uint8_t reg_addr, uint8_t *dat
                         LL_I2C_GENERATE_START_READ);
 
   for (uint32_t i = 0; i < length; i++) {
-    read_byte(handle, &data[i]);
+    ERR_CHECK(read_byte(handle, &data[i]));
   }
 
   while (!LL_I2C_IsActiveFlag_STOP(handle));
   LL_I2C_ClearFlag_STOP(handle);
 
-  return 0;
+  return RET_OK;
 }
 
 void i2c_init(i2c_t *instance, void *hw_i2c_handle, LL_I2C_InitTypeDef *config)
