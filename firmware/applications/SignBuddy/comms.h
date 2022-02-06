2f600dc82fe3c2d1d8ac9cb1d5b853b5b74b2789
Stephen Scott <stephen99scott@gmail.com>
2022-02-06 14:27:08 -0500

    Remove function prototypes

--- firmware/applications/SignBuddy/comms.h
+++ firmware/applications/SignBuddy/comms.h
@@ -7,8 +7,5 @@ void comms_task_setup(void);
 void comms_task_start(void);
 
 void comms_tx_data(Sample *);
-void comms_rx_data_ready_cb(void);
-void comms_tx_empty_cb(void);
-void comms_tx_data_clear_cb(void);
 
 #endif // COMMS_H
