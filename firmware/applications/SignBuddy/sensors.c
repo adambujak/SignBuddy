098762922ab04754fd7295bc32098c3fd1b1517e
Adam Bujak <adamjbujak@gmail.com>
2022-02-07 17:47:52 -0500

    improve sensors data collection
    format
    
    revert proto
    
    add log back

--- firmware/applications/SignBuddy/sensors.c
+++ firmware/applications/SignBuddy/sensors.c
@@ -25,7 +25,6 @@
 #define SAMPLING_PERIOD    50
 #define MAX_SAMPLES        40
 
-
 typedef struct {
   TaskHandle_t       sensors_task_handle;
   TimerHandle_t      sampling_timer;
@@ -45,9 +44,35 @@ static void flex_data_ready_cb(void)
   xEventGroupSetBits(s.data_ready_event_group, FLEX_DR_EVENT);
 }
 
-void sampling_timer_cb(TimerHandle_t xTimer)
+static void sampling_timer_cb(TimerHandle_t xTimer)
 {
   xTaskNotifyGive(s.sensors_task_handle);
+}
+
+static void touch_data_get(void)
+{
+  int8_t tsc_vals[TOUCH_SENSOR_CNT];
+
+  tsc_get_value(tsc_vals);
+  for (int i = 0; i < TOUCH_SENSOR_CNT; i++) {
+    LOG_DEBUG("touch sensor val %d: %d\r\n", i, (int) tsc_vals[i]);
+  }
+  s.sample.touchData.touch1 = tsc_vals[0];
+  s.sample.touchData.touch2 = tsc_vals[1];
+  s.sample.touchData.touch3 = tsc_vals[2];
+}
+
+static void flex_data_get(void)
+{
+  uint16_t flex_val;
+  uint32_t *flex_ptr = (uint32_t *) &s.sample.flexData;
+
+  for (uint8_t i = 0; i < FLEX_SENSOR_CNT; i++) {
+    flex_get_value(&flex_val, i);
+    *flex_ptr = flex_val;
+    LOG_DEBUG("Flex_%hu val: %lu\r\n", i + 1, *flex_ptr);
+    flex_ptr++;
+  }
 }
 
 static void sensors_task(void *arg)
@@ -95,21 +120,8 @@ static void sensors_task(void *arg)
     else {
       LOG_DEBUG("Sample_id: %lu\r\n", s.sample.sample_id);
 
-      int8_t tsc_vals[TOUCH_SENSOR_CNT];
-      tsc_get_value(tsc_vals);
-      for (int i = 0; i < TOUCH_SENSOR_CNT; i++) {
-        LOG_DEBUG("touch sensor val %d: %d\r\n", i, (int) tsc_vals[i]);
-      }
-
-      uint16_t flex_val;
-      uint32_t *flex_ptr = (uint32_t *) &s.sample.flexData;
-
-      for (uint8_t i = 0; i < FLEX_SENSOR_CNT; i++) {
-        flex_get_value(&flex_val, i);
-        *flex_ptr = flex_val;
-        LOG_DEBUG("Flex_%hu val: %lu\r\n", i + 1, *flex_ptr);
-        flex_ptr++;
-      }
+      touch_data_get();
+      flex_data_get();
     }
     comms_tx_data(&s.sample);
     s.sample.sample_id++;
