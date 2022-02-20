c70a94692f8c096ff13fbe82a747b05270e4a7e5
Stephen Scott <55762060+stephen99scott@users.noreply.github.com>
2022-02-20 15:37:51 -0500

    Add protobuf to app and training data collection (#129)
    * Update gradle
    
    * Update dependencies
    
    * Add Sign Buddy proto file and protobuf module
    
    * Formatting proto
    
    * Replace with updated proto
    
    * Add protobuf parsing functionality
    
    * Added gesture builder
    
    * Add lock on potentially shared data
    
    * Save training data to file
    
    * Add commands for single sample or periodic sampling
    
    * Capitalize constants, clarify sampling once vs periodic

--- software/app/src/main/java/com/example/signbuddy/MainActivity.java
+++ software/app/src/main/java/com/example/signbuddy/MainActivity.java
@@ -12,9 +12,11 @@ import android.bluetooth.BluetoothGatt;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
@@ -22,41 +24,67 @@ import com.clj.fastble.BleManager;
 import com.clj.fastble.callback.BleGattCallback;
 import com.clj.fastble.callback.BleNotifyCallback;
 import com.clj.fastble.callback.BleScanCallback;
+import com.clj.fastble.callback.BleWriteCallback;
 import com.clj.fastble.data.BleDevice;
 import com.clj.fastble.exception.BleException;
 import com.clj.fastble.scan.BleScanRuleConfig;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final int FINE_LOCATION_CODE = 1;
+    private final String BLE_NAME = "Sign Buddy BLE";
+    private final String UUID_SERVICE = "6E400001-B5A3-F393-E0A9-E50E24DCCA9E";
+    private final String UUID_CHARACTERISTIC_WRITE = "6E400002-B5A3-F393-E0A9-E50E24DCCA9E";
+    private final String UUID_CHARACTERISTIC_NOTIFY = "6E400003-B5A3-F393-E0A9-E50E24DCCA9E";
+    private final byte[] CMD_SAMPLE_ONCE = {0x01};
+    private final byte[] CMD_SAMPLE_PERIODIC = {0x02};
+    private final int MAX_SAMPLES = 40;
+    private final byte MSG_SYNC = 0x16;
+    private final byte MID_SAMPLE = 0x01;
+
     private Button connectButton;
     private ProgressBar connectProgress;
-    private final int FINE_LOCATION_CODE = 1;
+    private Button collectButton;
     private BleDevice SignBuddy;
-    private final String ble_name = "Sign Buddy BLE";
-    private final String uuid_service = "6E400001-B5A3-F393-E0A9-E50E24DCCA9E";
-    private final String uuid_characteristic_notify = "6E400003-B5A3-F393-E0A9-E50E24DCCA9E";
-    private int byteCount = 0;
+    private ReentrantLock samplesLock = new ReentrantLock();
+    private List<SignBuddyProto.SBPSample> samples = new ArrayList<>();
+    private ByteArrayOutputStream byteBuffer = new ByteArrayOutputStream();
+    private int num_samples;
+    private int msg_length;
+    private int skip_crc;
+    private boolean next_byte_is_length;
+    private String gestureLetter = "DEFAULT";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button lessonButton = (Button) findViewById(R.id.lessonButton);
+        Button lessonButton = findViewById(R.id.lessonButton);
         lessonButton.setOnClickListener(this::openLessonActivity);
 
-        Button quizButton = (Button) findViewById(R.id.quizButton);
+        Button quizButton = findViewById(R.id.quizButton);
         quizButton.setOnClickListener(this::openQuizActivity);
 
         connectProgress = findViewById(R.id.connectProgress);
         connectButton = findViewById(R.id.connectButton);
         connectButton.setOnClickListener(this::connectButtonCallback);
 
+        collectButton = findViewById(R.id.collectButton);
+        collectButton.setOnClickListener(this::collectButtonCallback);
+
         BleManager.getInstance().init(getApplication());
         BleManager.getInstance()
-                .enableLog(true)
+                .enableLog(false)
                 .setReConnectCount(1, 5000)
                 .setOperateTimeout(5000);
 
@@ -98,6 +126,39 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    private void collectButtonCallback(View v) {
+        EditText collectLetter = findViewById(R.id.collectLetter);
+        gestureLetter = collectLetter.getText().toString();
+        if (gestureLetter.matches("")) {
+            Toast.makeText(this, "No letter specified!", Toast.LENGTH_SHORT).show();
+        } else {
+            collectButton.setEnabled(false);
+            collectButton.setText(R.string.collecting);
+            byte[] cmd;
+            // If the letter is J or Z then we must do periodic sampling to track movement
+            if ("jzJZ".contains(gestureLetter)) {
+                num_samples = MAX_SAMPLES;
+                cmd = CMD_SAMPLE_PERIODIC;
+            } else {
+                num_samples = 1;
+                cmd = CMD_SAMPLE_ONCE;
+            }
+            BleManager.getInstance().write(SignBuddy, UUID_SERVICE, UUID_CHARACTERISTIC_WRITE, cmd, new BleWriteCallback() {
+                @Override
+                public void onWriteSuccess(int current, int total, byte[] justWrite) {
+                    Toast.makeText(MainActivity.this, "Starting collection!", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onWriteFailure(BleException exception) {
+                    Toast.makeText(MainActivity.this, "Failed to start collection!", Toast.LENGTH_SHORT).show();
+                    collectButton.setEnabled(true);
+                    collectButton.setText(R.string.collect);
+                }
+            });
+        }
+    }
+
     private void connectButtonCallback(View v) {
         if (connectButton.getText().equals(getString(R.string.enable_bluetooth))) {
             connectButton.setEnabled(false);
@@ -128,7 +189,7 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onScanning(BleDevice bleDevice) {
                     String name = bleDevice.getName();
-                    if (name != null && name.equals(ble_name)) {
+                    if (name != null && name.equals(BLE_NAME)) {
                         SignBuddy = bleDevice;
                         BleManager.getInstance().connect(SignBuddy, new BleGattCallback() {
                             @Override
@@ -149,7 +210,7 @@ public class MainActivity extends AppCompatActivity {
                                 Toast.makeText(MainActivity.this, "Connected!", Toast.LENGTH_SHORT).show();
                                 connectButton.setText(R.string.connected);
                                 connectProgress.setVisibility(View.GONE);
-                                BleManager.getInstance().notify(SignBuddy, uuid_service, uuid_characteristic_notify, new BleNotifyCallback() {
+                                BleManager.getInstance().notify(SignBuddy, UUID_SERVICE, UUID_CHARACTERISTIC_NOTIFY, new BleNotifyCallback() {
                                     @Override
                                     public void onNotifySuccess() {
 
@@ -162,13 +223,23 @@ public class MainActivity extends AppCompatActivity {
 
                                     @Override
                                     public void onCharacteristicChanged(byte[] data) {
-                                        StringBuilder sb = new StringBuilder();
                                         for (byte b : data) {
-                                            sb.append(String.format("%02X ", b));
-                                            byteCount++;
+                                            if (b == MSG_SYNC && byteBuffer.size() == 0) {
+                                                skip_crc = 4;
+                                                next_byte_is_length = true;
+                                            } else if (next_byte_is_length) {
+                                                msg_length = b;
+                                                next_byte_is_length = false;
+                                            } else if (skip_crc > 0) {
+                                                skip_crc--;
+                                            } else {
+                                                byteBuffer.write(b);
+                                                if (byteBuffer.size() == msg_length) {
+                                                    new Thread(new ParseMessage(byteBuffer.toByteArray())).start();
+                                                    byteBuffer.reset();
+                                                }
+                                            }
                                         }
-                                        Log.i("Sign Buddy", sb.toString());
-                                        Log.i("Sign Buddy", String.valueOf(byteCount));
                                     }
                                 });
                             }
@@ -209,6 +280,63 @@ public class MainActivity extends AppCompatActivity {
                 connectButton.setVisibility(View.VISIBLE);
             } else {
                 Toast.makeText(this, "Permission DENIED", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private class ParseMessage implements Runnable {
+
+        private byte[] msg;
+
+        public ParseMessage(byte[] msg) {
+            this.msg = msg;
+        }
+
+        public void run() {
+            try {
+                SignBuddyProto.SBPMessage message = SignBuddyProto.SBPMessage.parseFrom(this.msg);
+                if (message.getId() == MID_SAMPLE) {
+                    SignBuddyProto.SBPSample sample = message.getSample();
+                    Log.i("Sign Buddy", "NEW SAMPLE");
+                    samplesLock.lock();
+                    samples.add(sample);
+                    Log.i("Sign Buddy", String.valueOf(samples.size()));
+                    if (samples.size() == num_samples) {
+                        SignBuddyProto.SBPGestureData gestureData = SignBuddyProto.SBPGestureData.newBuilder()
+                                .setLetter(gestureLetter)
+                                .addAllSamples(samples)
+                                .build();
+                        Log.i("SignBuddy", gestureData.toString());
+
+                        File file = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+
+                        file = new File(file, "sign_buddy_training_data.txt");
+
+                        if (!file.exists()) {
+                            file.createNewFile();
+                        }
+
+                        PrintWriter writer = new PrintWriter(new OutputStreamWriter(new FileOutputStream(file, true)));
+                        writer.println(gestureData);
+                        writer.println("####################################################");
+                        writer.println("####################################################");
+                        writer.println("####################################################");
+
+                        writer.flush();
+                        writer.close();
+
+                        samples.clear();
+                        runOnUiThread(() -> {
+                            Toast.makeText(MainActivity.this, "Gesture collected!", Toast.LENGTH_SHORT).show();
+                            collectButton.setEnabled(true);
+                            collectButton.setText(R.string.collect);
+                        });
+                        Log.i("Sign Buddy", "NEW GESTURE");
+                    }
+                    samplesLock.unlock();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
     }
