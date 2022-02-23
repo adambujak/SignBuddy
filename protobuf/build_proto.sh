7f7b327bae1c593ccc74d116227767bab22894b3
Adam Bujak <adamjbujak@gmail.com>
2022-02-23 14:13:26 -0500

    protobuf abstraction (#142)
    * move protobuf to highest level
    
    * run formatter
    
    * remove format step from build_proto
    
    * Add protoc dependency in docker image
    
    Co-authored-by: Stephen Scott <stephen99scott@gmail.com>

---
+++ protobuf/build_proto.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+IMAGE_NAME="buildenv"
+
+GIT_ROOT=$(git rev-parse --show-toplevel)
+FIRMWARE_ROOT=$GIT_ROOT/firmware
+NANOPB_ROOT=$FIRMWARE_ROOT/dependencies/nanopb/generator
+PROTO_PATH=$GIT_ROOT/protobuf
+
+C_GENERATE_PATH=$FIRMWARE_ROOT/applications/SignBuddy
+PYTHON_GENERATE_PATH=$GIT_ROOT/software/sidekick
+
+pushd $C_GENERATE_PATH > /dev/null
+cp $PROTO_PATH/sign_buddy.proto .
+python3 $NANOPB_ROOT/nanopb_generator.py sign_buddy.proto
+rm sign_buddy.proto
+popd
+
+pushd $PYTHON_GENERATE_PATH > /dev/null
+cp $PROTO_PATH/sign_buddy.proto .
+protoc -I=. --python_out=. sign_buddy.proto
+rm sign_buddy.proto
+popd
