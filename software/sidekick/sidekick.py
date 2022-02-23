27b0d8402cb4dbc05e2cf970265790ba479aa061
Adam Bujak <adamjbujak@gmail.com>
2022-02-23 16:33:36 -0500

    added decoder, crc wrong

--- software/sidekick/sidekick.py
+++ software/sidekick/sidekick.py
@@ -4,9 +4,87 @@ import time
 import signal
 import serial
 import serial.tools.list_ports
+import zlib
 from threading import Thread, Lock, Event
+from enum import Enum
 
 sidekick = None
+
+SYNC_BYTE = 0x16
+FRAME_HEADER_SIZE = 6 # SYNC + LENGTH + 4 CRC
+
+class DecoderParseState(Enum):
+    HEADER = 0
+    BODY = 1
+
+# SignBuddy Stream Decoder
+class SBSDecoder():
+    def __init__(self, callback):
+        self.encoded = bytearray()
+        self.state = DecoderParseState.HEADER
+        self.currentFrameSize = 0
+        self.callback = callback
+        self.decodedCRC = 0
+
+    def _decode(self):
+        while True:
+            if self.state == DecoderParseState.HEADER:
+                if len(self.encoded) < FRAME_HEADER_SIZE:
+                    return
+
+                if self.encoded[0] != SYNC_BYTE:
+                    self.encoded = self.encoded[1:]
+                    continue
+                print('sync found')
+
+                self.currentFrameSize = self.encoded[1]
+                self.state = DecoderParseState.BODY
+
+                self.decodedCRC = 0
+                self.decodedCRC |= self.encoded[2] << 24
+                self.decodedCRC |= self.encoded[3] << 16
+                self.decodedCRC |= self.encoded[4] << 8
+                self.decodedCRC |= self.encoded[5] << 0
+
+                # pop out entire header
+                self.encoded = self.encoded[FRAME_HEADER_SIZE:]
+
+            if self.state == DecoderParseState.BODY:
+                if len(self.encoded) < self.currentFrameSize:
+                    return
+
+                frameData = self.encoded[0:self.currentFrameSize]
+                calculatedCRC = int(zlib.crc32(frameData))
+
+                print('decodedCRC: ', hex(self.decodedCRC))
+                print('calculatedCRC: ', hex(calculatedCRC))
+                if self.decodedCRC != calculatedCRC:
+                    self._soft_reset()
+                    continue
+
+                # shift out data
+                self.encoded = self.encoded[self.currentFrameSize:]
+                # reset for next packet
+                self._soft_reset()
+
+                #pass to decoder
+                self.callback(frameData)
+
+    def _soft_reset(self):
+        self.state = DecoderParseState.HEADER
+        self.currentFrameSize = 0
+        self.decodedCRC = 0
+
+    def decode(self, encoded):
+        self.encoded = self.encoded + encoded
+        self._decode()
+
+    def reset(self):
+        self.encoded = bytearray()
+        self.state = DecoderParseState.HEADER
+        self.currentFrameSize = 0
+        self.decodedCRC = 0
+
 
 class SerialDevice:
     def __init__(self, serialPort, baudRate=115200, readSleepTimeMs=1):
@@ -64,18 +142,28 @@ class SerialDevice:
         self.serialInstance.write(byteData)
         self.serialMutex.release()
 
+    def read_raw_char(self):
+        self.serialMutex.acquire()
+        readval = self.serialInstance.read(1)
+        self.serialMutex.release()
+        return readval
+
     def read_callback(self):
         # User should overwrite this in their own class
         pass
 
 class Sidekick(SerialDevice):
-        def read_callback(self, byteData):
+    def read_callback(self, byteData):
         self.decoder.decode(byteData)
 
     def write_packet(self, packet):
-        frame = self.encoder.pack_packets_into_frame([packet])
-        self.write_bytes(frame)
         time.sleep(0.001)
+
+    def decoder_init(self):
+        self.decoder = SBSDecoder(self.decoder_callback)
+
+    def decoder_callback(self):
+        print('frame complete')
 
 def quit():
     global sidekick
@@ -83,7 +171,7 @@ def quit():
     del sidekick
     exit(0)
 
-def main(update_att_values, update_alt_values, update_yaw_values):
+def main():
     global sidekick
 
     connected = False
@@ -117,3 +205,6 @@ def main(update_att_values, update_alt_values, update_yaw_values):
         serialPort = ports[selectedPort].device
 
         sidekick = Sidekick(serialPort, 115200, 1)
+        sidekick.decoder_init()
+
+main()
