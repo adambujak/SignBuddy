1ebeae071d89f19308617afecac0d2c4e763d9e5
Stephen Scott <55762060+stephen99scott@users.noreply.github.com>
2022-02-23 19:19:17 -0500

    Move proto javalite compilation to higher level (#147)
    * Add java option to .proto
    
    * Compile proto for javalite at higher level
    
    * Remove protobuf module from android app
    
    * Add software readme placeholder

--- protobuf/build_proto.sh
+++ protobuf/build_proto.sh
@@ -4,11 +4,13 @@ IMAGE_NAME="buildenv"
 
 GIT_ROOT=$(git rev-parse --show-toplevel)
 FIRMWARE_ROOT=$GIT_ROOT/firmware
+SOFTWARE_ROOT=$GIT_ROOT/software
 NANOPB_ROOT=$FIRMWARE_ROOT/dependencies/nanopb/generator
 PROTO_PATH=$GIT_ROOT/protobuf
 
 C_GENERATE_PATH=$FIRMWARE_ROOT/applications/SignBuddy
-PYTHON_GENERATE_PATH=$GIT_ROOT/software/sidekick
+PYTHON_GENERATE_PATH=$SOFTWARE_ROOT/sidekick
+JAVA_GENERATE_PATH=$SOFTWARE_ROOT/android_app/app/src/main/java
 
 pushd $C_GENERATE_PATH > /dev/null
 cp $PROTO_PATH/sign_buddy.proto .
@@ -19,5 +21,11 @@ popd
 pushd $PYTHON_GENERATE_PATH > /dev/null
 cp $PROTO_PATH/sign_buddy.proto .
 protoc -I=. --python_out=. sign_buddy.proto
+rm sign_buddy.proto
+popd
+
+pushd $JAVA_GENERATE_PATH > /dev/null
+cp $PROTO_PATH/sign_buddy.proto .
+protoc -I=. --javalite_out=. sign_buddy.proto
 rm sign_buddy.proto
 popd
